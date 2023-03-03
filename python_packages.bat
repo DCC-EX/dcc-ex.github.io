397ed54a03f2f2db80c44dc78af11f7d49a910c2
peteGSX <97784652+peteGSX@users.noreply.github.com>
2023-03-03 10:18:24 +1000

    Fix version

--- python_packages.bat
+++ python_packages.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
 ECHO Installing Python packages...
-pip3 install sphinx-rtd-theme==1.2.0 sphinx-sitemap sphinxcontrib-spelling sphinx-toolbox==3.4.0
+pip3 install sphinx-rtd-theme==1.2.0 sphinx-sitemap sphinxcontrib-spelling sphinx-toolbox

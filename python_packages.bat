aa535b625c94673bb40ad72db6f68a37e9e9a903
peteGSX <97784652+peteGSX@users.noreply.github.com>
2023-03-03 10:18:03 +1000

    Update packages to match workflow

--- python_packages.bat
+++ python_packages.bat
@@ -1,9 +1,4 @@
 @ECHO OFF
 
 ECHO Installing Python packages...
-pip3 install docutils==0.18
-pip3 install sphinx-rtd-theme==1.0.0
-pip3 install sphinx-sitemap
-pip3 install sphinxcontrib-spelling
-pip3 install sphinx-toolbox==3.4.0
-pip3 install sphinx==5.3.0
+pip3 install sphinx-rtd-theme==1.2.0 sphinx-sitemap sphinxcontrib-spelling sphinx-toolbox==3.4.0

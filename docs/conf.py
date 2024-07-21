f07190e3fa847880a8b932c54c3a3854738e5082
Peter Akers <akersp62@gmail.com>
2024-07-21 13:59:27 +1000

    small clarification to the EX-RAIL reference page

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ extensions = [
     # 'sphinx.ext.graphviz',
     'sphinx_sitemap',
     # 'sphinx.ext.inheritance_diagram',
-    'sphinxcontrib.spelling',
+    # 'sphinxcontrib.spelling',
     'sphinx_toolbox.collapse',
     'ablog',
     'sphinx.ext.intersphinx',
@@ -107,7 +107,7 @@ html_theme_options = {
     'titles_only': True,
     'collapse_navigation': False,
     # 'navigation_depth': 3
-     'navigation_depth': -1 
+    'navigation_depth': -1
 }
 
 html_context = {
@@ -117,7 +117,8 @@ html_context = {
     'github_version': 'sphinx/docs/',
 }
 
-# leave this off to have todos invisible, set to true to render them and make the easy to see
+# leave this off to have todos invisible,
+# set to true to render them and make the easy to see.
 # A list of all the todos in the document can be shown in the about page
 todo_include_todos = True
 
@@ -131,7 +132,7 @@ html_css_files = [
     'css/sphinx_design_overrides.css',
     # 'css/layout.css',
     # 'css/roundslider.min.css',
-    # 'css/throttle.css' 
+    # 'css/throttle.css'
 ]
 
 html_js_files = [
@@ -167,8 +168,10 @@ redirects = {
     "about/press/new-name": "https://dcc-ex.com/news/posts/20201001.html",
     "about/rewrite": "https://dcc-ex.com/news/posts/20201001.html",
     "advanced-setup/index": "../ex-commandstation/advanced-setup/index.html",
-    "advanced-setup/supported-microcontrollers/index": "../../advanced-setup/supported-microcontrollers/index.html",
-    "advanced-setup/supported-motorboards/index": "../../reference/hardware/motor-boards.html",
+    "advanced-setup/supported-microcontrollers/index":
+    "../../advanced-setup/supported-microcontrollers/index.html",
+    "advanced-setup/supported-motorboards/index":
+    "../../reference/hardware/motor-boards.html",
     "automation/EX-RAIL-intro": "../ex-rail/index.htm",
     "automation/index": "../ex-rail/index.htm",
     "begin/*": "../ex-commandstation/get-started/index.html",
@@ -181,36 +184,48 @@ redirects = {
     "download/documents": "index.html",
     "download/schematics": "index.html",
     "download/turntable-ex": "ex-turntable.html",
-    "ex-commandstation/advanced-setup/supported-bluetooth/index": "../../../reference/hardware/bluetooth.html",
+    "ex-commandstation/advanced-setup/supported-bluetooth/index":
+    "../../../reference/hardware/bluetooth.html",
     "ex-installer/installing-original-installer": "index.html",
     "ex-rail/deprecate-EX-RAIL-reference": "index.html",
     "ex-rail/deprecate-EX-RAIL-summary": "index.html",
     "exwebthrottle/LICENSE": "../about/licence.html",
     "get-started/assembly": "../ex-commandstation/get-started/assembly.html",
-    "get-started/controllers": "../ex-commandstation/get-started/controllers.html",
+    "get-started/controllers":
+    "../ex-commandstation/get-started/controllers.html",
     "get-started/index": "../ex-commandstation/get-started/index.html",
     "get-started/installer": "../ex-commandstation/get-started/installer.html",
     "get-started/levels": "../begin/levels.html",
-    "get-started/wifi-setup": "../ex-commandstation/get-started/wifi-setup.html",
+    "get-started/wifi-setup":
+    "../ex-commandstation/get-started/wifi-setup.html",
     "glossary": "./reference/glossary.html",
     "news/posts/20230310": "../../news.html",
     "news/posts/20230408": "../../news.html",
     "news/posts/20230806": "../../news.html",
     "press/index": "../../news.html",
     "press/v40-announce": "../../news.html",
-    "reference/developers/tech-reference": "../../throttles/tech-reference.html",
-    "reference/documents/decoder-table": "../../reference/hardware/decoder-list.html",
-    "reference/hardware/bluetooth-boards": "../../reference/hardware/bluetooth.html",
+    "reference/developers/tech-reference":
+    "../../throttles/tech-reference.html",
+    "reference/documents/decoder-table":
+    "../../reference/hardware/decoder-list.html",
+    "reference/hardware/bluetooth-boards":
+    "../../reference/hardware/bluetooth.html",
     "reference/hardware/other/index": "../../reference/hardware/index.html",
-    "reference/hardware/shopping-list": "../../ex-commandstation/get-started/purchasing.html",
-    "reference/software/command-summary": "../../reference/software/command-summary-consolidated.html",
-    "reference/software/single-opcode-reference": "../../reference/software/command-summary-consolidated.html",
+    "reference/hardware/shopping-list":
+    "../../ex-commandstation/get-started/purchasing.html",
+    "reference/software/command-summary":
+    "../../reference/software/command-summary-consolidated.html",
+    "reference/software/single-opcode-reference":
+    "../../reference/software/command-summary-consolidated.html",
     "site-map/index": "../about/site-map.html",
     "support/create-ticket": "../support/index.html",
-    "support/makerfabs-update-at-version-with-usb-to-ttl": "../support/wifi-at-version.html#correcting-the-at-version-on-a-makerfabs-esp8266-wifi-shield",
+    "support/makerfabs-update-at-version-with-usb-to-ttl":
+    "../support/wifi-at-version.html#correcting-the-at-version-on-a-makerfabs-esp8266-wifi-shield",
     "throttles/cab-engineer": "../throttles/software/cab-engineer.html",
-    "throttles/hardware/physical-knobs": "../../throttles/hardware/engine-driver-physical-knobs.html",
-    "throttles/physical-knobs": "../throttles/hardware/engine-driver-physical-knobs.html",
+    "throttles/hardware/physical-knobs":
+    "../../throttles/hardware/engine-driver-physical-knobs.html",
+    "throttles/physical-knobs":
+    "../throttles/hardware/engine-driver-physical-knobs.html",
     "throttles/software/ex-webthrottle": "../../ex-webthrottle/index.html",
     "throttles/witcontroller": "../throttles/hardware/witcontroller.html",
     "throttles/withrottle": "../throttles/software/withrottle.html",
@@ -227,7 +242,8 @@ notfound_context = {
 
 <p>Sorry, we couldn't find that page.</p>
 
-<p>Try using the search box or go to the <a href="/index.html">homepage</a>.</p>
+<p>Try using the search box or go to the
+<a href="/index.html">homepage</a>.</p>
 """,
 }
 notfound_urls_prefix = "/dcc-ex.github.io/"

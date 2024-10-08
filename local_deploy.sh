fe320e276adaa599b1b51575e05c9c4d8014478c
Harald Barth <haba@kth.se>
2024-10-08 16:31:11 +0200

    local_deploy, but as shell script

---
+++ local_deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+## hint for haba:
+##/usr/bin/kinit -t /etc/krb5.keytab host/bento.stacken.kth.se bash -c 'chown haba `echo $KRB5CCNAME  | sed s/FILE://1` ; su haba bash -c "afslog -c stacken.kth.se; tokens; touch foo; kdestroy"'
+
+# runs only on haba's deploy:
+type -p afslog > /dev/null && afslog
+
+# here is the real start
+##########################
+
+# check if we already have a venv prepared (if yes, we use that)
+if test -d venv ; then
+    : all well
+else
+    python3 -m venv venv || exit 255
+fi
+
+# activate prepared venv
+. venv/bin/activate
+# install requirements (why are those not in requirements.txt?)
+pip3 install sphinx-rtd-theme==1.2.0 sphinx-sitemap sphinxcontrib-spelling sphinx-toolbox ablog sphinx-design==0.3.0 || exit 255
+
+while sleep 1; do
+
+    git fetch --dry-run 2>&1 | awk ' $4 != "" { print "touch ../branch/"$2"/update-me"}' | bash
+
+    for DIR in ../branch/*/update-me ; do
+	BRANCH=`echo $DIR | awk -F/ '{print $3}'`
+	#BRANCH=`git branch | egrep '^\*' | sed 's/. //1'`
+	git checkout "$BRANCH"
+	git pull
+	pip3 install -r requirements.txt || exit 255
+	sphinx-build -M html docs/. "../branch/$BRANCH/" -W --keep-going 2>&1 | cat > "../branch/$BRANCH/lastrun.log"
+	rm "../branch/$BRANCH/update-me"
+    done
+    # still in testing, break here
+    break
+done
+
+#deactivate venv
+deactivate
+
+exit 0
